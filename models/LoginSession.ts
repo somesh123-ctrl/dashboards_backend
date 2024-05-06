51666c109f5768364823c9272d470c157f13d071
somesh123-ctrl <55444715+somesh123-ctrl@users.noreply.github.com>
2024-05-07 01:25:01 +0530

    Update LoginSession.ts

--- models/LoginSession.ts
+++ models/LoginSession.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-// Define interface for login session document
+
 export interface ILoginSession extends Document {
   email: string;
   deviceType: string;
@@ -9,7 +9,7 @@ export interface ILoginSession extends Document {
 
 }
 
-// Define schema for login session
+
 const LoginSessionSchema: Schema = new Schema({
   email: { type: String, required: true },
   deviceType: { type: String, required: true },
@@ -17,5 +17,5 @@ const LoginSessionSchema: Schema = new Schema({
   // logoutAt: { type: Date, default: null }
 });
 
-// Define and export the LoginSession model
+
 export default mongoose.model<ILoginSession>('LoginSession', LoginSessionSchema);
