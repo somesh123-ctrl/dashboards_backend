c435c72c6190327a56a9b84152bf6cfe35b9f2fc
somesh123-ctrl <55444715+somesh123-ctrl@users.noreply.github.com>
2024-05-07 01:24:34 +0530

    Update LoginLogoutActivity.ts

--- models/LoginLogoutActivity.ts
+++ models/LoginLogoutActivity.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-// Define interface for login session document
+
 export interface ILoginLogoutActivity extends Document {
   email: string;
   deviceType: string;
@@ -8,7 +8,7 @@ export interface ILoginLogoutActivity extends Document {
   logoutAt: Date;
 }
 
-// Define schema for login session
+
 const LoginLogoutActivitySchema: Schema = new Schema({
   email: { type: String, required: true },
   deviceType: { type: String, required: true },
@@ -16,5 +16,4 @@ const LoginLogoutActivitySchema: Schema = new Schema({
   logoutAt: { type: Date, default: null }
 });
 
-// Define and export the LoginSession model
 export default mongoose.model<ILoginLogoutActivity>('LoginLogoutActivity', LoginLogoutActivitySchema);
