3b2abffbf46aea554209b0f2979f542d1511f6a6
somesh123-ctrl <55444715+somesh123-ctrl@users.noreply.github.com>
2024-05-07 01:20:17 +0530

    Update User.ts

--- models/User.ts
+++ models/User.ts
@@ -3,13 +3,13 @@ import mongoose, { Document } from 'mongoose';
 export interface IUser extends Document {
   email: string;
   password: string;
-  otp: string;  // Define the secret property as optional
+  otp: string;  
 }
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  otp: { type: String } // Define the secret property
+  otp: { type: String } 
 });
 
 const User = mongoose.model<IUser>('User', userSchema);
